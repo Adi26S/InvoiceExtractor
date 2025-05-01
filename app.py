5df0dd958a88519ffc880273034a1882927c5eba
Adi26s <adishukla514@gmail.com>
2025-05-01 11:44:04 +0530

    changes the use_columns_width with use_container_width in order to avoid deprecation error

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ uploaded_file = st.file_uploader('choose an image of the invoice..',type=['jpg',
 image=''
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
-    st.image(image, caption='Uploaded image', use_column_width=True)
+    st.image(image, caption='Uploaded image', use_container_width=True)
     
 submit = st.button('Fetch info')
 
